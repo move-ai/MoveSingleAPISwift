27761573db462795f1bce5d60f54b1759c073cb6
flxfischer <flx.fischer@gmail.com>
2023-08-02 10:36:22 +0200

    Add MocabFeed and Protobuf Generator

---
+++ Sources/MoveSingleAPISwift/Protobuf/ProtobufGenerator.swift
@@ -0,0 +1,258 @@
+//
+//  File.swift
+//  
+//
+//  Created by Felix Fischer on 31/07/2023.
+//
+
+import Foundation
+import AVFoundation
+
+final class ProtobufGenerator {
+
+    struct Configuration {
+        let camera: Camera
+        let includeIMUData: Bool
+        let includeLidarData: Bool
+        let useDeviceMotionUserAcceleration: Bool
+
+        enum Camera: String, CaseIterable, Identifiable, CustomStringConvertible, Equatable, Codable {
+            case front = "Front"
+            case back = "Back"
+            case backLiDAR = "Back+LiDAR"
+
+            var id: Self { self }
+            var description: String { self.rawValue }
+            var pbCameraName: String {
+                switch self {
+                case .front:
+                    return "iPhoneFrontRGB"
+                case .back:
+                    return "iPhoneBackRGB"
+                case .backLiDAR:
+                    return "iPhoneBackLidar"
+                }
+            }
+        }
+    }
+
+    static func generate(from frames: [Frame], config: Configuration) async throws -> Data {
+        return try await Task {
+            var observations = Observations()
+            var depthSequence = DepthSequence()
+
+            if let lastFrame = frames.last {
+                depthSequence.camera = try camera(from: lastFrame, config: config)
+            }
+
+            frames.forEach { frame in
+                var odometryInstance = OdometryInstance()
+
+                if let calibrationData = frame.enhancementData.depthSensorData?.depthData.cameraCalibrationData {
+                    odometryInstance.coordinateSystem.transform = transform(from: calibrationData)
+                }
+
+                if config.includeIMUData, let cameraPositionData = frame.enhancementData.cameraPositionData {
+                    odometryInstance.imu = imu(from: cameraPositionData, config: config)
+                }
+
+                depthSequence.camera.odometry.trajectory.append(odometryInstance)
+
+                if config.includeLidarData, let depthData = frame.enhancementData.depthSensorData?.depthData {
+                    depthSequence.frames.append(depthMap(from: depthData))
+                }
+            }
+            observations.lidars.append(depthSequence)
+
+            let data = try observations.serializedData()
+            return data
+        }.value
+    }
+
+    private static func camera(from frame: Frame, config: Configuration) throws -> Camera {
+        var camera = Camera()
+        camera.name = config.camera.pbCameraName
+        camera.intrinsics = intrinsic(from: frame.sampleBuffer)
+        if let calibrationData = frame.enhancementData.depthSensorData?.depthData.cameraCalibrationData {
+            camera.extrinsics = extrinsics(from: calibrationData)
+        }
+        return camera
+    }
+
+    private static func intrinsic(from sampleBuffer: CMSampleBuffer) -> Camera.Intrinsics {
+        var height: Int = 0
+        var width: Int = 0
+        if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            height = CVPixelBufferGetHeight(imageBuffer)
+            width = CVPixelBufferGetWidth(imageBuffer)
+        }
+
+        var intrinsicMatrix = matrix_float3x3()
+        if let camData = CMGetAttachment(sampleBuffer,
+                                         key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix,
+                                         attachmentModeOut: nil) as? Data {
+            intrinsicMatrix = camData.withUnsafeBytes { $0.pointee }
+        }
+
+        var intrinsics = Camera.Intrinsics()
+        intrinsics.centerPointX = intrinsicMatrix[2][0]
+        intrinsics.centerPointY = intrinsicMatrix[2][1]
+
+        intrinsics.focalLengthX = intrinsicMatrix[0][0]
+        intrinsics.focalLengthY = intrinsicMatrix[1][1]
+
+        intrinsics.skew = intrinsicMatrix[0][1]
+        intrinsics.resolution = Camera.Intrinsics.Resolution()
+        intrinsics.resolution.width = Int32(width)
+        intrinsics.resolution.height = Int32(height)
+
+        return intrinsics
+    }
+
+    private static func extrinsics(from calibrationData: AVCameraCalibrationData) -> CoordinateSystem {
+        let extrinsicMatrix = calibrationData.extrinsicMatrix
+
+        var extrinsics = CoordinateSystem()
+        // Translation
+        extrinsics.transform = Transform()
+        extrinsics.transform.translation = Translation()
+
+        extrinsics.transform.translation.x = extrinsicMatrix[3][0]
+        extrinsics.transform.translation.y = extrinsicMatrix[3][1]
+        extrinsics.transform.translation.z = extrinsicMatrix[3][2]
+        // Rotation
+        extrinsics.transform.orientation = Orientation()
+        extrinsics.transform.orientation.rotationType = .matrix
+        extrinsics.transform.orientation.rotationValues = []
+
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[0][0])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[0][1])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[0][2])
+
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[1][0])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[1][1])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[1][2])
+
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[2][0])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[2][1])
+        extrinsics.transform.orientation.rotationValues.append(extrinsicMatrix[2][2])
+
+        return extrinsics
+    }
+
+    private static func transform(from calibrationData: AVCameraCalibrationData) -> Transform {
+        var trace: Float = 0.0
+        var quat: vector_float4 = vector_float4()
+
+        let mat = calibrationData.extrinsicMatrix
+
+        if mat[2][2] < 0 {
+            if mat[0][0] > mat[1][1] {
+                trace = 1 + mat[0][0] - mat[1][1] - mat[2][2]
+                quat = vector_float4(trace, mat[0][1] + mat[1][0], mat[2][0] + mat[0][2], mat[1][2] - mat[2][1])
+            } else {
+                trace = 1 - mat[0][0] + mat[1][1] - mat[2][2]
+                quat = vector_float4(mat[0][1] + mat[1][0], trace, mat[1][2] + mat[2][1], mat[2][0] - mat[0][2])
+            }
+        } else {
+            if mat[0][0] < -mat[1][1] {
+                trace = 1 - mat[0][0] - mat[1][1] + mat[2][2]
+                quat = vector_float4(mat[2][0] + mat[0][2], mat[1][2] + mat[2][1], trace, mat[0][1] - mat[1][0])
+            } else {
+                trace = 1 + mat[0][0] + mat[1][1] + mat[2][2]
+                quat = vector_float4(mat[1][2] - mat[2][1], mat[2][0] - mat[0][2], mat[0][1] - mat[1][0], trace)
+            }
+        }
+
+        let traceInverse = 1.0 / (sqrt(trace))
+        quat *= traceInverse
+
+        var transform = Transform()
+
+        transform.translation = Translation()
+        transform.translation.x = mat[3][0]
+        transform.translation.y = mat[3][1]
+        transform.translation.z = mat[3][2]
+
+        transform.orientation = Orientation()
+        transform.orientation.rotationValues = [quat.x, quat.y, quat.z, quat.w]
+
+        return transform
+    }
+
+    private static func imu(from cameraPositionData: CameraPositionData, config: Configuration) -> IMU {
+
+        let accVector = config.useDeviceMotionUserAcceleration ? cameraPositionData.deviceMotion.userAcceleration : cameraPositionData.accelerometerData.acceleration //cameraData.deviceMotionUserAccelerationVector : cameraData.accelorometerVector
+
+        var imu = IMU()
+        imu.gyrX = Float(cameraPositionData.deviceMotion.attitude.pitch) // x
+        imu.gyrY = Float(cameraPositionData.deviceMotion.attitude.roll) // y
+        imu.gyrZ = Float(cameraPositionData.deviceMotion.attitude.yaw) // z
+        imu.accX = Float(accVector.x)
+        imu.accY = Float(accVector.y)
+        imu.accZ = Float(accVector.z)
+        imu.magX = Float(cameraPositionData.magnetometerData.magneticField.x)
+        imu.magY = Float(cameraPositionData.magnetometerData.magneticField.y)
+        imu.magZ = Float(cameraPositionData.magnetometerData.magneticField.z)
+        imu.grvX = Float(cameraPositionData.gyroData.rotationRate.x)
+        imu.grvY = Float(cameraPositionData.gyroData.rotationRate.y)
+        imu.grvZ = Float(cameraPositionData.gyroData.rotationRate.z)
+
+        return imu
+
+    }
+
+    private static func depthMap(from depthData: AVDepthData) -> DepthMap {
+        let height = CVPixelBufferGetHeight(depthData.depthDataMap)
+        let width = CVPixelBufferGetWidth(depthData.depthDataMap)
+
+        var depthMap = DepthMap()
+        depthMap.height = Int32(height)
+        depthMap.width = Int32(width)
+
+        let depthFloatArray = convert(buffer: depthData.depthDataMap)
+        let accuracy = depthData.depthDataAccuracy
+        let quality = Float(depthData.depthDataQuality.rawValue)
+
+        if height >= 1 && width >= 1 {
+            for row in 0...height - 1 {
+                var pbRow = DepthMap.row()
+                var conRow = DepthMap.row()
+                for col in 0...width - 1 {
+                    let depthRow = depthFloatArray[(row * width) + col]
+                    pbRow.row.append(depthRow)
+                    if accuracy == .absolute {
+                        conRow.row.append(quality)
+                    } else {
+                        conRow.row.append(0)
+                    }
+                }
+                depthMap.map.append(pbRow)
+                depthMap.confidence.append(conRow)
+            }
+        }
+
+        return depthMap
+    }
+
+    private static func convert(buffer: CVPixelBuffer) -> [Float32] {
+        guard let baseAddress = getBaseAddress(for: buffer) else { return [] }
+        let sourceDataSize = CVPixelBufferGetDataSize(buffer)
+        let width = CVPixelBufferGetWidth(buffer)
+        let height = CVPixelBufferGetHeight(buffer)
+
+        let floatPtr = baseAddress.bindMemory(to: Float32.self, capacity: sourceDataSize / 4 )
+        let floatBufferPtr = UnsafeBufferPointer(start: floatPtr, count: width * height )
+
+        let array = Array(floatBufferPtr)
+
+        return array
+    }
+
+    private static func getBaseAddress(for buffer: CVPixelBuffer) -> UnsafeMutableRawPointer? {
+        CVPixelBufferLockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress: UnsafeMutableRawPointer? = CVPixelBufferGetBaseAddress(buffer)
+        CVPixelBufferUnlockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        return baseAddress
+    }
+}
