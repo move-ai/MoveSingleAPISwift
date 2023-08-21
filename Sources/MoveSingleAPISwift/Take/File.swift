0df00767f75c379454d3bebbd1a0ec7f56f7b81c
tommoveai <116635912+tommoveai@users.noreply.github.com>
2023-08-21 10:41:39 +0200

    Format code

--- Sources/MoveSingleAPISwift/Take/File.swift
+++ Sources/MoveSingleAPISwift/Take/File.swift
@@ -71,12 +71,12 @@ public actor File: Equatable, Hashable {
         try await urlSessionClient.download(url: presignedURL, to: toURL)
         localFileName = remoteID
     }
-	
-	public func generateShareCode() async throws -> String {
-		guard let remoteID = remoteID else { throw FileError.remoteIdMissing }
-		let shareCode = try await graphQLClient.generateShareCode(fileId: remoteID)
-		return shareCode.code
-	}
+    
+    public func generateShareCode() async throws -> String {
+        guard let remoteID = remoteID else { throw FileError.remoteIdMissing }
+        let shareCode = try await graphQLClient.generateShareCode(fileId: remoteID)
+        return shareCode.code
+    }
 
     public struct CodableFile: Codable {
         let id: UUID
